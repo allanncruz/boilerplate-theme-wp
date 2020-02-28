62abbd9b968003d689dd074b9a7e86434a4a29dc
allanncruz <contato@allancruz.com.br>
2020-02-27 23:48:08 -0300

    add fontawesom
    update gitignore

--- functions/theme_scripts.php
+++ functions/theme_scripts.php
@@ -3,10 +3,9 @@ add_action('wp_enqueue_scripts', 'theme_scripts', 'favicon');
 function theme_scripts()
 {
     wp_enqueue_style('main style', get_template_directory_uri() . '/assets/styles/main.css');
+    wp_enqueue_style('fontawesome',  'https://use.fontawesome.com/releases/v5.5.0/css/all.css');
+
     wp_enqueue_script('scripts', get_template_directory_uri() . '/assets/scripts/main.js');
-
-
-    
     wp_enqueue_script('jquery', get_template_directory_uri() . '/resources/node_modules/jquery/dist/jquery.min.js');
     wp_enqueue_script('popper.js', get_template_directory_uri() . '/resources/node_modules/popper.js/dist/umd/popper.min.js');
     wp_enqueue_script('bootstrap', get_template_directory_uri() . '/resources/node_modules/bootstrap/dist/js/bootstrap.min.js');
